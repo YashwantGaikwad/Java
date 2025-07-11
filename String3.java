80ed90c042443f5ce3e62134fcfed3ab2537312a
Yashwant Gaikwad <yashwantg9696@gmail.com>
2025-07-11 17:12:50 +0530

    learn string

---
+++ String3.java
@@ -0,0 +1,11 @@
+public class String3 {
+    public static void main(String[] args) {
+        // Declare without new 
+        String name = "Yashwant Gaikwad";
+        System.out.println(name);
+
+        //Declare with new 
+        String friend = new String("AbhayRaje Patil");
+        System.out.println(friend);
+    }
+}
---
+++ String4.java
@@ -0,0 +1,11 @@
+public class String4 {
+    public static void main(String[] args) {
+        char charaters[]={'A','B','H','A','Y','R','A','J','E'};
+        String name = new String(charaters);
+
+        String name2 = new String("Patil");
+        
+        System.out.println(name);
+        System.out.println(name2);
+    }
+}
